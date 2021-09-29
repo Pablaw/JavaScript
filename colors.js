5a1e1e257ced951f5f8dd269b4b6fba77565a0d7
Pablaw <chulhwa7@gmail.com>
2021-09-29 16:34:25 +0900

    Wed leave work

--- colors.js
+++ colors.js
@@ -1,34 +1,38 @@
 var links = {
     setColor:function(color){
-  let alist = document.querySelectorAll('a');
-  let i = 0;
-  while( i < alist.length){
-  alist[i].style.color = color;
-  i = i + 1;
-  }
+  //  let alist = document.querySelectorAll('a');
+  //  let i = 0;
+  //  while( i < alist.length){
+  //  alist[i].style.color = color;
+  //  i = i + 1;
+  //  }
+  // 반복문을 jQuery로 처리할 수 있다.
+  $('a').css('color', color);
   }
   }
 
 var Body = {
 setColor:function (color){
-document.querySelector('body').style.color = color;
+// document.querySelector('body').style.color = color;
+$('body').css('color', color);
 },
 setBackgroundColor:function (color){
-document.querySelector('body').style.backgroundColor= color;
+// document.querySelector('body').style.backgroundColor= color;
+$('body').css('backgroundColor', color);
 }
 }
 function nightDayHandler(self){
-        let target =  document.querySelector('body');
-    if(self.value === 'night'){
-      Body.setBackgroundColor('black');
-      Body.setColor('white');
-      self.value = 'day';
+  let target =  document.querySelector('body');
+if(self.value === 'night'){
+Body.setBackgroundColor('black');
+Body.setColor('white');
+self.value = 'day';
 links.setColor('powderblue');
-      }
-    else{
-      Body.setBackgroundColor('white');
-      Body.setColor('black');
-      self.value = 'night';
+}
+else{
+Body.setBackgroundColor('white');
+Body.setColor('black');
+self.value = 'night';
 links.setColor('blue');
-      }
-  }
+}
+}
