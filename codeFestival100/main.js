b2702cf53bbd2388c5c2825e58a078572b935ad0
Pablaw <chulhwa7@gmail.com>
2021-10-25 17:14:25 +0900

    Oct 4th Mon Leave Work

--- codeFestival100/main.js
+++ codeFestival100/main.js
@@ -32,3 +32,44 @@ for(var i = 1; i < 5; i += 2) {
 }
 console.log(a1 + b1);
 // 4) 16
+// Q6. False O
+// NaN, 1, "", 0, undefined 중 Falthy가 아닌 것은? 1
+// null, undefined, 0, 빈 문자열, NaN, false를 제외한 것은 모두 truthy
+// Q7. 변수명으로 사용할 수 없는 것 X
+// age, &age, let, _age, 1age -> &age, let?
+// JS 식별자는 문자, 밑줄(_), 달러기호($)로 시작해야 한다. let은 예약어로 사용불가.
+// Q8. 객체의 키 이름 중복 X
+// 78, 84? -> 중복 시 가장 나중(아래값)값이 출력 됨.
+var d = {
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'weight':92,
+    'temperature':36,
+    'eyesight':1
+};
+console.log(d['weight']);
+
+// Q9. concat을 활용한 출력 방법
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+
+console.log(result);
+// 출력 목표값 2019/04/26 11:34:27
+// concat() 메서드는 매개변수로 전달된 문자열을 메서드를 호출한 문자열에 붙여 새로운 문자열로 반환.
+// Q10. 별찍기
+// 입력 값: 5, 별찍기로 5단 트리만들기
+
+let n2 = 5;
+for(let j = 1; j <= n2; j++){
+    
+}
+
+// for(let j = 1; j <= 10; j += 2){
+// console.log(' '.repeat((10-j)/2) + '*'.repeat(j));
+// }
