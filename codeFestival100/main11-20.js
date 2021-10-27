00477ff106d757eaa5f9887e9eb2b89909ae9216
Pablaw <chulhwa7@gmail.com>
2021-10-27 18:01:35 +0900

    Oct 4th Wed Leave work

--- codeFestival100/main11-20.js
+++ codeFestival100/main11-20.js
@@ -25,4 +25,20 @@ x.attack();
 
 let n = prompt('1~8 중에서 숫자를 입력하세요','');
 let SolarSystem = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성'];
+
 console.log(`입력한 순서에 해당하는 태양계 해성은 ${SolarSystem[n - 1]}입니다.`);
+// Q.14 3의 배수 인가요?
+let n2 = prompt('숫자를 입력하세요','');
+if(n % 3 == 0) {
+    console.log("짝");
+} else {console.log(`${n2}`)}
+// Q.15 자기소개
+let name = prompt('이름을 입력하세요.', '');
+console.log(`안녕하세요. 저는 ${name}입니다.`);
+//Q.16 로꾸거 -> split / reverse / join
+let n3 = prompt('단어를 입력하세요.', '');
+const reverse = n3.split('').reverse().join('');
+console.log(reverse);
+
+
+
