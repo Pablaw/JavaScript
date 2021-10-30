9c7969663dea155fbc3a6c13276634f5ffcab1ea
Pablaw <chulhwa7@gmail.com>
2021-10-30 23:44:01 +0900

    Oct 4th Sat Night3

--- codeFestival100/main21-30.js
+++ codeFestival100/main21-30.js
@@ -5,3 +5,9 @@
 //Q22. 배수인지 확인하기
 // i가 6의 배수인지 확인하기
 // i % 6 == 0 ?? 
+//Q23. OX 문제
+console.log(Math.floor(10/3));
+//Q24. 대문자로 바꾸기
+let name = prompt('영문이름을 입력해주세요','');
+let upper = name.toUpperCase();
+console.log(upper);
