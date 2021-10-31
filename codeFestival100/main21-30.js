55b1ad2fb984a8c6263a9520e6978632fe66ac0a
Pablaw <chulhwa7@gmail.com>
2021-10-31 21:34:57 +0900

    Oct 5th Sun night

--- codeFestival100/main21-30.js
+++ codeFestival100/main21-30.js
@@ -6,8 +6,13 @@
 // i가 6의 배수인지 확인하기
 // i % 6 == 0 ?? 
 //Q23. OX 문제
-console.log(Math.floor(10/3));
-//Q24. 대문자로 바꾸기
-let name = prompt('영문이름을 입력해주세요','');
-let upper = name.toUpperCase();
-console.log(upper);
+// console.log(Math.floor(10/3));
+// //Q24. 대문자로 바꾸기
+// let name = prompt('영문이름을 입력해주세요','');
+// let upper = name.toUpperCase();
+// console.log(upper);
+//Q25. 원의 넓이 구하기.
+// 원의 넓이 = 반지름 * 반지름 * 3.14
+let n = prompt('숫자 정수를 입력해주세요.','');
+let area = (n ** n) * 3.14;
+console.log(area);
