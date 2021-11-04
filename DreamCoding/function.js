7396ada97d80df64da49f2f9ba6fe3dab2c8b741
Pablaw <chulhwa7@gmail.com>
2021-11-04 17:50:34 +0900

    Nov 1st Thu Leave Work

--- DreamCoding/function.js
+++ DreamCoding/function.js
@@ -5,14 +5,38 @@
 // command: add, substract, divide, multiply, remainder
 const a = prompt('숫자를 입력하세요.', '');
 const b = prompt('숫자를 입력하세요.', '');
-const command = prompt('영문으로 연산자를 입력하세요.','');
+const command = prompt('연산자를 입력하세요.','');
 
-
-let add = (a, b) => a + b;
-
-
-console.log(command);
-
-
+// function calculate(command, a, b){
+// switch (command) {
+//     case '+':
+//         return a + b;
+//     case '-':
+//         return a - b;
+//     case '/':
+//         return a / b;
+//     case '*':
+//         return a * b;
+//     case '%':
+//         return a % b;
+    
+//     default:
+//         throw Error ('unknown command');
+//     }
+// }
+function calculate(command, a, b){
+    if(command === 'add') {
+        return a + b;
+    } else if(command === 'substract') {
+        return a - b;
+    } else if(command === 'divide') {
+        return a / b;
+    } else if (command === 'multiply') {
+        return a * b;
+    } else if (command === 'remainder') {
+        return a % b;
+    } else { throw Error ('unknown command')}
+}
+console.log(calculate(command, +a, +b));
 
 
