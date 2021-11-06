4dcea01fefe3f8bc173e455c44b97117a6f09cb8
Pablaw <chulhwa7@gmail.com>
2021-11-06 14:44:47 +0900

    Nov 1st Sat 1st Counter

--- test/main.js
+++ test/main.js
@@ -7,13 +7,18 @@ const upBtn = document.querySelector(".upBtn");
 downBtn.addEventListener('click', downClick);
 upBtn.addEventListener('click', upClick);
 
-let n = parseInt(display.value, 10);
+let n = 0;
 
 function downClick () {
-   display.value -= 1;
-};
+    if (n > 0) {
+        n -= 1
+        display.value = n;
+    } else {
+        n = 0;
+    }
+}
 
 function upClick () {
-    display.value += 1;
-};
-console.log(n);
+        n += 1;
+        display.value = n;
+    }
