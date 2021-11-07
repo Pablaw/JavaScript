adcd05122eb8c8a0ad566de445eb5b3a5e5f55ef
Pablaw <chulhwa7@gmail.com>
2021-11-07 22:06:43 +0900

    Nov 2nd Sun Succeed

--- test/main.js
+++ test/main.js
@@ -1,1 +1,22 @@
 'usestrict';
+const downBtn = document.querySelector('.downBtn');
+const upBtn = document.querySelector('.upBtn');
+const display = document.querySelector('.display');
+
+let n = 0;
+
+downBtn.addEventListener('click', downClick);
+upBtn.addEventListener('click', upClick);
+
+function upClick () {
+    n++;
+    display.value = n;
+}
+function downClick () {
+    if(n > 0) {
+        n--;
+    } else {
+        n = 0;
+    }
+    display.value = n;
+}
