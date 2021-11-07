10423209c49f9fa9355dd8e513ccf537d67694a8
Pablaw <chulhwa7@gmail.com>
2021-11-07 22:11:55 +0900

    Nov 2nd Sun add resetBtn

--- test/main.js
+++ test/main.js
@@ -2,11 +2,13 @@
 const downBtn = document.querySelector('.downBtn');
 const upBtn = document.querySelector('.upBtn');
 const display = document.querySelector('.display');
+const reset = document.querySelector('.reset');
 
 let n = 0;
 
 downBtn.addEventListener('click', downClick);
 upBtn.addEventListener('click', upClick);
+reset.addEventListener('click', resetClick);
 
 function upClick () {
     n++;
@@ -18,5 +20,9 @@ function downClick () {
     } else {
         n = 0;
     }
+    display.value = n;
+}
+function resetClick () {
+    n = 0;
     display.value = n;
 }
