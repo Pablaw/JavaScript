eb7828518cad37ae11dcc60cb875a83d02825696
Pablaw <chulhwa7@gmail.com>
2021-11-18 22:46:44 +0900

    Nov 3rd Thu Night2

--- Browser101/bounding.js
+++ Browser101/bounding.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log('a');
+
+const div = document.querySelectorAll('div');
+console.log(div);
+
+// for(let i=0; i < div.length; i++) {
+div[0].addEventListener('click', () => {
+    div[0] = div.getBoundingClientRect();
+});
+
+
+
+// div.addEventListener('click', () => {
+//     div.getBoundingClientRect();
+// })
+
