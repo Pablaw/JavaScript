ef44e19126a73f3c21413837ebda4b9363533901
Pablaw <chulhwa7@gmail.com>
2021-11-19 17:55:10 +0900

    Nov 3rd Fri Leave Work

--- codeFestival100/main51-60.js
+++ codeFestival100/main51-60.js
@@ -1,36 +1,61 @@
 // Q51. merge sort 만들기
-function mergeSort(arr){
-    if (arr.length <= 1){
-      return arr;
-    }
+// function mergeSort(arr){
+//     if (arr.length <= 1){
+//       return arr;
+//     }
   
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0,mid);
-    const right = arr.slice(mid);
+//     const mid = Math.floor(arr.length / 2);
+//     const left = arr.slice(0,mid);
+//     const right = arr.slice(mid);
   
-    return merge(mergeSort(left), mergeSort(right));
+//     return merge(mergeSort(left), mergeSort(right));
+//   }
+  
+//   function merge(left, right){
+//     let result = [];
+  
+//     while (left.length && right.length){
+//       if (left[0] < right[0]){
+//         result.push(left.shift());
+//       } else {
+//         result.push(right.shift());
+//       }
+//     }
+//     while (left.length) {
+//       result.push(left.shift());
+//     }
+//     while (right.length) {
+//       result.push(right.shift());
+//     }
+  
+//     return result;
+//   }
+  
+//   const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+  
+//   console.log(mergeSort(array));
+
+// Q52. quick sort
+function quickSort(arr){
+  if (arr.length <= 1){
+    return arr;
   }
-  
-  function merge(left, right){
-    let result = [];
-  
-    while (left.length && right.length){
-      if (left[0] < right[0]){
-        result.push(left.shift());
-      } else {
-        result.push(right.shift());
-      }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i=1; i<arr.length; i++){
+    if(arr[i] < pivot){
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
-    while (left.length) {
-      result.push(left.shift());
-    }
-    while (right.length) {
-      result.push(right.shift());
-    }
-  
-    return result;
   }
-  
-  const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
-  
-  console.log(mergeSort(array));
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+
+const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+
+console.log(quickSort(array));//
+
